db4eeda9e4f4fc0285152526664a3d1248552b10
Abhishek <sharmaabhi91011@gmail.com>
2021-01-21 15:38:16 +0530

    Initial commit

---
+++ src/Literals.java
@@ -0,0 +1,23 @@
+public class Literals {
+    public static void main(String[] args){
+        byte age=34;
+        char ch='a';
+        int count=69;
+        short num=123;
+        long temp=100000000000L;
+        float a=5.6f;
+        double b=4.66556555;
+        boolean flag=true;
+        String str="Hello";
+        System.out.println(age);
+        System.out.println(ch);
+        System.out.println(count);
+        System.out.println(num);
+        System.out.println(temp);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(flag);
+        System.out.println(str);
+
+    }
+}
